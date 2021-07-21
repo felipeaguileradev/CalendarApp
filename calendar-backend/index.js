0e9b1b8aa69824c9a55e6bfd0993927f31c0b2cd
felipe0703 <felipe.aguilera.g@gmail.com>
2021-07-21 00:04:32 -0400

    creacion de backen con node, express, mongo

--- calendar-backend/index.js
+++ calendar-backend/index.js
@@ -1,1 +1,27 @@
-console.log("hola mundoooo");
+const express = require("express");
+const { dbConnection } = require("./database/config");
+const cors = require("cors");
+require("dotenv").config();
+
+// crear el servidor de express
+const app = express();
+
+// Base de datos
+dbConnection();
+
+// CORS
+app.use(cors());
+
+// Directorio publico
+app.use(express.static("public")); //es un middleware, se inicia cuando se sube el servidor
+
+// lectura y parseo del body
+app.use(express.json()); // pasa por este middleware
+
+// Rutas
+app.use("/api/auth", require("./routes/auth"));
+
+// escuchar peticiones
+app.listen(process.env.PORT, () => {
+  console.log(`Servidor corriendo en puerto ${process.env.PORT}`);
+});
